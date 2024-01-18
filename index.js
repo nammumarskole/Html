8bdbf8b5c6659e9841890dc2f7a09d8baa7ce38e
Naman Marskole <153010733+nammumarskole@users.noreply.github.com>
2024-01-18 19:46:55 +0530

    msg

---
+++ index.js
@@ -0,0 +1,17 @@
+//  var firstname = 'ram'
+//  var lastname = 'sita'
+//  console.log(firstname+" "+lastname)
+//  console.log('firstname: ${firstname} lastname ${lastname}')
+
+// let value = 100
+// let to = value==100
+// console.log(to);
+
+function sum(){
+    console.log('hello');
+}
+sum()
+const add = () => {
+    console.log('hii');
+}
+ 
